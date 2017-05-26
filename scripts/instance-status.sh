6985b5485e227469e77f8858c4667862d3dec4c7
Tim Hartmann <tfhartmann@gmail.com>
2017-05-26 15:46:11 -0400

    Update to status script

--- scripts/instance-status.sh
+++ scripts/instance-status.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//')
-STATUS=$(aws ec2 describe-instance-status --region $REGION --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id) | jq -r '.InstanceStatuses[0].InstanceStatus.Status')
+ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+ASG=$(aws ec2 describe-instances --region $REGION --instance-ids $ID | jq -r '.Reservations[0].Instances[0].Tags[1].Value')
+STATUS=$(aws autoscaling describe-auto-scaling-instances --instance-ids $ID --region $REGION | jq -r '.AutoScalingInstances[0].LifecycleState')
 
-if [ $STATUS = "ok" ]; then
+if [ $STATUS = "InService" ]; then
   echo InstanceStatus is : $STATUS
 else
   echo InstanceStatus is : $STATUS
