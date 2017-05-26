3cc40a07ffb6aa24499707547bfb0672ada2b048
Tim Hartmann <tfhartmann@gmail.com>
2017-05-26 11:00:00 -0400

    Cause I can't cut/paste?

--- scripts/instance-status.sh
+++ scripts/instance-status.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone/ | sed 's/.$//')
-STATUS=$(aws ec2 describe-instance-status --region $REGION --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id) | jq -r '.I
+STATUS=$(aws ec2 describe-instance-status --region $REGION --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id) | jq -r '.InstanceStatuses[0].InstanceStatus.Status')
 
 if [ $STATUS = "ok" ]; then
   echo InstanceStatus is : $STATUS
