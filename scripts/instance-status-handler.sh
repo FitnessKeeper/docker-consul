8ce199488284d1d28faed05a133e61226e08d20f
Tim Hartmann <tfhartmann@gmail.com>
2017-05-26 16:56:20 -0400

    Hacky hack-hack

--- scripts/instance-status-handler.sh
+++ scripts/instance-status-handler.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+# We probably want to replace this with something that eats json and do this in a non-horrible way
 
 sleep 10
-/bin/consul leave
+/usr/local/bin/instance-status.sh
+
+RC=$?
+
+if [ $RC = 255 ]; then
+  /bin/consul leave
+else
+  echo InstanceStatus is ok
+  exit 0
+fi
